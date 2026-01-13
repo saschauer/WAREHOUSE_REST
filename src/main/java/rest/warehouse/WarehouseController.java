cf7a17a09ee15536b6f149cb2c686cd1df0237be
saschauer <aschauer.stefan@proton.me>
2026-01-13 08:56:10 +0100

    - Added @CrossOrigin connection between frontend and backend

--- src/main/java/rest/warehouse/WarehouseController.java
+++ src/main/java/rest/warehouse/WarehouseController.java
@@ -1,5 +1,6 @@
 package rest.warehouse;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,26 +10,20 @@ import org.springframework.http.MediaType;
 import rest.model.WarehouseData;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WarehouseController {
 
     @Autowired
     private WarehouseService service;
-	
+
     @RequestMapping("/")
     public String warehouseMain() {
-    	return "This is the warehouse application! (DEZSYS_WAREHOUSE_REST) <br/><br/>" +
-                "<a href='http://localhost:8080/warehouse/001/json'>Link to warehouse/001/json</a><br/>" +
-                "<a href='http://localhost:8080/warehouse/001/xml'>Link to warehouse/001/xml</a><br/>";
+        return "This is the warehouse application! (DEZSYS_WAREHOUSE_REST) <br/><br/>" +
+                "<a href='http://localhost:8080/warehouse/001/json'>Link to warehouse/001/json</a><br/>" ;
     }
 
-    @RequestMapping(value="/warehouse/{inID}/json", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WarehouseData warehouseDataJSON( @PathVariable String inID ) {
-        return service.getWarehouseData( inID );
+    @RequestMapping(value="/warehouse/{inID}/data", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    public WarehouseData warehouseData( @PathVariable String inID ) {
+        return service.getWarehouseData(inID);
     }
-
-    @RequestMapping(value="/warehouse/{inID}/xml", produces = MediaType.APPLICATION_XML_VALUE)
-    public WarehouseData warehouseDataXML( @PathVariable String inID ) {
-        return service.getWarehouseData( inID );
-    }
-
 }
